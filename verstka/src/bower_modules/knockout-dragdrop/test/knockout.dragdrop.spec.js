090e196b6f5fe2612afbf3fe91f13930281d8add
Denis Berezin <nocab139@yandex.ru>
2017-10-10 22:54:49 +0300

    com10

--- verstka/src/bower_modules/knockout-dragdrop/test/knockout.dragdrop.spec.js
+++
@@ -1,7 +0,0 @@
-var expect = window.weknowhow.expect;
-
-describe('knockout.dragdrop', function () {
-    it('attaches the binding handlers', function () {
-        expect(ko.bindingHandlers, 'to have keys', ['dropZone', 'dragEvents', 'dragZone', 'scrollableOnDragOver']);
-    });
-});
